9554b000adfb0a8160f2bf5b394f24b6454f8e92
AndrewRademacher <andrewrademacher@gmail.com>
2017-09-21 11:43:43 -0500

    Closes gh-22.

--- app/Mallard.hs
+++ app/Mallard.hs
@@ -42,27 +42,17 @@ appOptionsParser = AppOptions
 data AppState
     = AppState
         { _statePostgreConnection     :: Pool.Pool
-        , _stateRootDirectory         :: Path Abs Dir
-        , _stateMigrationFiles        :: [Path Abs File]
-        , _statePlannedMigrationTable :: MigrationTable
-        , _stateAppliedMigrationTable :: MigrationTable
-        , _stateMigrationGraph        :: MigrationGraph
         }
 
 $(makeClassy ''AppState)
 
 instance HasPostgreConnection AppState where postgreConnection = statePostgreConnection
-instance HasRootDirectory AppState where rootDirectory = stateRootDirectory
-instance HasMigrationFiles AppState where migrationFiles = stateMigrationFiles
-instance HasPlannedMigrationTable AppState where plannedMigrationTable = statePlannedMigrationTable
-instance HasAppliedMigrationTable AppState where appliedMigrationTable = stateAppliedMigrationTable
-instance HasMigrationGraph AppState where migrationGraph = stateMigrationGraph
 
 main :: IO ()
 main = do
     appOpts <- execParser opts
     pool <- Pool.acquire (1, 30, appOpts ^. optionsPostgreSettings)
-    let initState = AppState pool $(mkAbsDir "/doesnt/exist") [] Map.empty Map.empty emptyMigrationGraph
+    let initState = AppState pool
 
     _ <- (flip runReaderT appOpts . flip runStateT initState) run
             `catchAll` (\e -> putStrLn (displayException e) >> return ((), initState))
@@ -79,19 +69,14 @@ run = do
     --
     appOpts <- ask
     root <- makeAbsolute =<< parseRelDir (appOpts ^. optionsRootDirectory . unpacked)
-    modify (\s -> s & rootDirectory .~ root)
     --
     files <- scanDirectoryForFiles root
-    modify (\s -> s & migrationFiles .~ files)
     --
     mPlanned <- importMigrations root files
-    modify (\s -> s & plannedMigrationTable .~ mPlanned)
     --
     mApplied <- getAppliedMigrations
-    modify (\s -> s & appliedMigrationTable .~ mApplied)
     --
     let mGraph = fromJust $ mkMigrationGraph mPlanned
-    modify (\s -> s & migrationGraph .~ mGraph)
     --
     ensureMigratonSchema
     --
@@ -102,19 +87,3 @@ run = do
     let unapplied = getUnappliedMigrations mGraph (Map.keys mApplied)
     applyMigrations mPlanned unapplied
     --
-
-    -- New application plan.
-
-    -- Add root folder to state (store in state)
-    -- Scan root directory for files (store in state)
-    -- Import migrations from files found (store in state) (planed migrations)
-    -- Import migrations from database (store in state) (applied migrations)
-    -- Generate migration graph (store in state) (merge nodeTable and graph into unified opaque type)
-    -- Find unapplied migrations (store in state)
-
-    -- Validate application state
-        -- Validate that all applied migrations exist in planned migrations.
-        -- Validate that all applied migrations have matching checksums with their planned migration counterparts.
-        -- Validate that there are no circular references in the migration graph.
-
-    -- Apply unapplied migrations.
