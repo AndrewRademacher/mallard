e8947bee8499ba7c2f4001683be8ed34c8df6e0d
AndrewRademacher <andrewrademacher@gmail.com>
2017-12-07 16:31:26 -0600

    Added build step to dep-package.sh.

--- scripts/deb-package.sh
+++ scripts/deb-package.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+cabal build
 mkdir -p /tmp/installdir/usr/local/bin
 cp dist/build/mallard/mallard /tmp/installdir/usr/local/bin/mallard
 fpm -s dir -t deb -n mallard -v 0.6.3.0 -C /tmp/installdir \
