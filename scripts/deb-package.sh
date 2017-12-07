34a60737c679ec05d7b01ad2777fe4290de4afde
AndrewRademacher <andrewrademacher@gmail.com>
2017-12-07 14:00:13 -0600

    Added package build to vagrant VM scripts.

---
+++ scripts/deb-package.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+mkdir -p /tmp/installdir/usr/local/bin
+cp dist/build/mallard/mallard /tmp/installdir/usr/local/bin/mallard
+fpm -s dir -t deb -n mallard -v 0.6.3.0 -C /tmp/installdir \
+        -d libpq5 \
+        usr/local/bin/mallard
--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -3,18 +3,27 @@ sudo apt-get upgrade -y
 sudo apt-get install -y software-properties-common build-essential
 sudo add-apt-repository -y ppa:hvr/ghc
 sudo apt-get update
-sudo apt-get install \
+sudo apt-get install -y \
     ghc-8.0.2 \
     cabal-install-2.0 \
     libpq-dev \
     postgresql \
-    happy -y
+    happy \
+    ruby \
+    ruby-dev \
+    rubygems \
+    build-essential
 sudo ln -s /opt/cabal/2.0/bin/cabal /usr/local/bin/cabal
 sudo ln -s /opt/ghc/8.0.2/bin/ghc /usr/local/bin/ghc
 
+# Project Dependencies
 cd /vagrant
 cabal update
 cabal install --only-dependencies --enable-tests
 
+# Database Setup
 sudo su postgres -c "psql -c \"CREATE ROLE vagrant WITH LOGIN SUPERUSER PASSWORD 'password';\""
 createdb vagrant
+
+# Packaging Tools
+# gem install -V --no-ri --no-rdoc fpm
