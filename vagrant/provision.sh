63054acb65e8aa7885ce75f27df3dde9e40332b7
AndrewRademacher <andrewrademacher@gmail.com>
2017-12-06 19:48:34 -0600

    GHC 8.2.2 fails to link, revert to 8.0.2.

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -1,38 +1,20 @@
-# sudo apt-get update
-# sudo apt-get upgrade -y
-# sudo apt-get install -y software-properties-common
-# sudo add-apt-repository -y ppa:hvr/ghc
-# sudo apt-get update
-# sudo apt-get install \
-#     ghc-8.2.2 \
-#     cabal-install-2.0 \
-#     libpq-dev \
-#     postgresql \
-#     happy -y
-# sudo ln -s /opt/cabal/2.0/bin/cabal /usr/local/bin/cabal
-# sudo ln -s /opt/ghc/8.2.2/bin/ghc /usr/local/bin/ghc
-
-# cd /vagrant
-# cabal update
-# cabal install --only-dependencies
-
-# sudo su postgres -c "psql -c \"CREATE ROLE vagrant WITH LOGIN SUPERUSER PASSWORD 'password';\""
-# createdb vagrant
-
-# New
-
 sudo apt-get update
 sudo apt-get upgrade -y
-sudo apt install -y \
+sudo apt-get install -y software-properties-common build-essential
+sudo add-apt-repository -y ppa:hvr/ghc
+sudo apt-get update
+sudo apt-get install \
+    ghc-8.0.2 \
+    cabal-install-2.0 \
     libpq-dev \
     postgresql \
-    curl
-
-curl -sSL https://get.haskellstack.org/ | sh
+    happy -y
+sudo ln -s /opt/cabal/2.0/bin/cabal /usr/local/bin/cabal
+sudo ln -s /opt/ghc/8.0.2/bin/ghc /usr/local/bin/ghc
 
 cd /vagrant
-stack setup
-stack build
+cabal update
+cabal install --only-dependencies
 
 sudo su postgres -c "psql -c \"CREATE ROLE vagrant WITH LOGIN SUPERUSER PASSWORD 'password';\""
 createdb vagrant
