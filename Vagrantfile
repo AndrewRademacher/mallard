3349070eb77bc3bc233283746248369c80e2ebab
AndrewRademacher <andrewrademacher@gmail.com>
2017-12-06 18:19:16 -0600

    Trial of stack over cabal for image building.

--- Vagrantfile
+++ Vagrantfile
@@ -2,7 +2,7 @@
 # vi: set ft=ruby :
 
 Vagrant.configure("2") do |config|
-    config.vm.box = "ubuntu/xenial64"
+    config.vm.box = "bento/ubuntu-16.04"
   
     config.vm.provider "virtualbox" do |vb|
       # Display the VirtualBox GUI when booting the machine
@@ -12,8 +12,18 @@ Vagrant.configure("2") do |config|
       vb.memory = "8000"
     end
 
-    config.vm.network "private_network", ip: "172.28.128.3"
+    config.vm.provider "vmware_fusion" do |vb|
+      vb.gui = false
+
+      vb.cpus = 7
+      vb.memory = "8000"
+    end
+
+    # config.vm.network "private_network", ip: "172.28.128.3"
+    config.vm.network "private_network", type: "dhcp"
   
+    # config.vm.synced_folder "", "/project"
+
     config.vm.provision "shell" do |s|
       s.path = "vagrant/provision.sh"
       s.privileged = false
--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -1,12 +1,38 @@
+# sudo apt-get update
+# sudo apt-get upgrade -y
+# sudo apt-get install -y software-properties-common
+# sudo add-apt-repository -y ppa:hvr/ghc
+# sudo apt-get update
+# sudo apt-get install \
+#     ghc-8.2.2 \
+#     cabal-install-2.0 \
+#     libpq-dev \
+#     postgresql \
+#     happy -y
+# sudo ln -s /opt/cabal/2.0/bin/cabal /usr/local/bin/cabal
+# sudo ln -s /opt/ghc/8.2.2/bin/ghc /usr/local/bin/ghc
+
+# cd /vagrant
+# cabal update
+# cabal install --only-dependencies
+
+# sudo su postgres -c "psql -c \"CREATE ROLE vagrant WITH LOGIN SUPERUSER PASSWORD 'password';\""
+# createdb vagrant
+
+# New
+
 sudo apt-get update
-sudo apt-get install -y software-properties-common
-sudo add-apt-repository -y ppa:hvr/ghc
-sudo apt-get update
-sudo apt-get install ghc-8.2.2 cabal-install-2.0 libpq-dev happy -y
-sudo ln -s /opt/cabal/2.0/bin/cabal /usr/local/bin/cabal
-sudo ln -s /opt/ghc/8.2.2/bin/ghc /usr/local/bin/ghc
+sudo apt-get upgrade -y
+sudo apt install -y \
+    libpq-dev \
+    postgresql \
+    curl
+
+curl -sSL https://get.haskellstack.org/ | sh
 
 cd /vagrant
-cabal sandbox init
-cabal update
-cabal install --only-dependencies
+stack setup
+stack build
+
+sudo su postgres -c "psql -c \"CREATE ROLE vagrant WITH LOGIN SUPERUSER PASSWORD 'password';\""
+createdb vagrant
